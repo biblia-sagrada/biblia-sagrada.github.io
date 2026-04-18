67d78a0a2201c1fa1e4944610014fa2e7cdd07b9
Palavra que fortifica <fabioxtrawork@gmail.com>
2026-04-18 19:39:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -31,9 +31,9 @@ function abrirLivro(i) {
     
     let html = '';
     if (livro.chapters && livro.chapters[0]) {
-        // Montamos uma sequência de spans sem quebras de linha entre eles
         livro.chapters[0].forEach((v, idx) => {
-            html += `<span class="versiculo"><span class="num-v">${idx + 1}</span>${v}</span> `;
+            // Mudamos para 'p' (parágrafo) para garantir a quebra de linha
+            html += `<p class="versiculo"><span class="num-v">${idx + 1}</span>${v}</p>`;
         });
     }
     document.getElementById('texto').innerHTML = html;
